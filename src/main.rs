b7927f611556e473d41945958450313b80da465c
Daniel Cardona <daniel.cardona@eagleview.com>
2020-02-07 11:44:21 -0500

    Connecting to a BE
    TODO implement marking task done

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod command;
 mod todo_item;
 mod todo_list;
 use command::*;
+use reqwest::header::HeaderName;
+use serde_json;
 use std::env;
 use std::result::*;
 use todo_item::*;
@@ -14,20 +16,35 @@ fn main() {
     let arguments: Vec<String> = env::args().collect();
     let command = Command::from(arguments);
 
+    // let url = "https://jsonplaceholder.typicode.com/todos";
+    let url = "https://todo-backend-rocket-rust.herokuapp.com";
+
     let client = reqwest::blocking::Client::new();
-    let response = client
-        .get("https://jsonplaceholder.typicode.com/todos")
+    let task_data = client
+        .get(url)
         .send()
         .expect("Something wrong fetching data from jsongplaceholder.typicode.com");
 
-    let tasks: Vec<TodoItem> = response.json().expect("Couldn't parse todo");
+    let tasks: Vec<TodoItem> = task_data.json().expect("Couldn't parse todo");
 
     let mut todo_list = TodoList { tasks: tasks };
 
     match command {
         Command::Get => todo_list.print(),
-        Command::Add(new_todo) => {
-            todo_list.add(TodoItem::new(new_todo));
+        Command::Add(new_item) => {
+            let new_todo = TodoItem::new(new_item);
+            let json_string = serde_json::to_string(&new_todo).expect("Could not serialize");
+            let response = client
+                .post("https://todo-backend-rocket-rust.herokuapp.com/")
+                .header("Content-Type", "application/json")
+                .body(json_string)
+                .send()
+                .expect("Couldn't create new todo");
+
+            let created_todo: TodoItem = response
+                .json()
+                .expect("Couldn't parse response to TodoItem");
+            todo_list.add(created_todo);
             todo_list.print();
         }
         Command::Done(index) => {
@@ -35,8 +52,15 @@ fn main() {
             todo_list.print();
         }
         Command::Remove(index) => {
-            todo_list.remove(index);
-            todo_list.print();
+            let task = todo_list.tasks[index].clone();
+            let delete_url = &task.id;
+            let response = client.delete(delete_url).send().unwrap();
+            if response.status().is_success() {
+                todo_list.remove(index);
+                println!("Removed task: {}", task);
+            } else {
+                println!("Failed to remove task: {}", task);
+            }
         }
     }
 }
--- src/todo_item.rs
+++ src/todo_item.rs
@@ -2,11 +2,15 @@ use serde::{Deserialize, Serialize};
 use std::fmt;
 use std::result::*;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TodoItem {
     #[serde(rename = "title")]
     pub name: String,
+
     pub completed: bool,
+
+    #[serde(rename = "url")]
+    pub id: String,
 }
 
 impl TodoItem {
@@ -14,6 +18,7 @@ impl TodoItem {
         return TodoItem {
             name: name,
             completed: false,
+            id: "".to_string(),
         };
     }
 }
