8d1614cc3ea8be942ab5a638dc9a0c2ff9c6c711
Daniel Cardona <daniel.cardona@eagleview.com>
2020-02-23 11:20:28 -0500

    Parsing command line args with clap

--- src/command.rs
+++ src/command.rs
@@ -1,20 +1,27 @@
+use clap::Clap;
+
+#[derive(Clap)]
 pub enum Command {
+    #[clap(about = "Lists all pending tasks")]
     Get,
-    Add(String),
-    Done(usize),
-    Remove(usize),
+    #[clap(about = "Add a new task to todo list")]
+    Add(Add),
+    #[clap(about = "Toggle complete on task")]
+    Done(Done),
+    Remove(Remove),
 }
 
-impl Command {}
+#[derive(Clap)]
+pub struct Add {
+    pub description: String,
+}
 
-impl From<Vec<String>> for Command {
-    fn from(args: Vec<String>) -> Self {
-        match args[1].clone().as_str() {
-            "get" => Command::Get,
-            "add" => Command::Add(args[2].clone()),
-            "done" => Command::Done(args[2].parse().unwrap()),
-            "remove" => Command::Remove(args[2].parse().unwrap()),
-            _ => panic!("Must provide a proper command"),
-        }
-    }
+#[derive(Clap, Debug)]
+pub struct Done {
+    pub index: usize,
+}
+
+#[derive(Clap)]
+pub struct Remove {
+    pub index: usize,
 }
--- src/main.rs
+++ src/main.rs
@@ -3,41 +3,51 @@ mod data_manager;
 mod todo_item;
 mod todo_list;
 
+use clap;
+use clap::Clap;
 use command::*;
 use data_manager::*;
 use std::env;
+use std::fmt::Debug;
 use todo_item::*;
 use todo_list::*;
 
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Daniel Cardona R.")]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: Command,
+}
+
 fn main() {
-    let arguments: Vec<String> = env::args().collect();
-    let command = Command::from(arguments);
+    let opts: Opts = Opts::parse();
 
     let url = "https://todo-backend-rocket-rust.herokuapp.com";
     let mut remote_data_manager = RemoteDataManager::new(url.to_string());
 
-    let mut todo_list = TodoList { tasks: vec![] };
+    let list = remote_data_manager.list();
+    let mut todo_list = TodoList { tasks: list };
 
-    match command {
+    return match opts.subcmd {
         Command::Get => {
             let list = remote_data_manager.list();
             let list = TodoList { tasks: list };
             todo_list = list;
             todo_list.print();
         }
-        Command::Add(new_item) => {
-            let created_todo = remote_data_manager.create(new_item);
+        Command::Add(cmd) => {
+            let created_todo = remote_data_manager.create(cmd.description);
             todo_list.add(created_todo);
             todo_list.print();
         }
-        Command::Done(index) => {
-            let mut task = todo_list.tasks[index].clone();
+        Command::Done(cmd) => {
+            let mut task = todo_list.tasks[cmd.index].clone();
             let done = task.completed;
             remote_data_manager.mark(&mut task, !done);
         }
-        Command::Remove(index) => {
-            let task = todo_list.tasks[index].clone();
+        Command::Remove(cmd) => {
+            let task = todo_list.tasks[cmd.index].clone();
             remote_data_manager.remove(task);
         }
-    }
+    };
 }
